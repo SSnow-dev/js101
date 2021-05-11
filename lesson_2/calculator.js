a37f5d28f51e0ae22d246dea655ddda4bcf95d8c
Steven Snow <scs9886@gmail.com>
2021-05-10 19:01:56 -0700

    finish refactor assignment for calculator program

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,9 +1,5 @@
 const readline = require('readline-sync');
 
-// I re-aranged the code a bit to work better in a loop, for convenience.
-// added validation check for operation input.
-
-//TODO add validation check for number input, 
 //TODO change output message to also display which operation was performed (IE "The result of 5 plus 6 is: 11")
 
 console.log("Welcome to Calculator!");
@@ -13,43 +9,73 @@ let number2;
 let output;
 
 while (operation !== '5') {
-  console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide 5) Exit");
+  prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide 5) Exit");
   operation = readline.question();
 
   if (operation === '5') {
-    console.log("Closing calculator");
+    notice("Closing calculator");
     break;
   }
 
   // check if operation input is valid
-  if (operation !== '1' && operation !== '2' && operation !== '3' && operation !== '4' && operation !== '5') {
-    console.log("Sorry, that's not a valid choice.");
+  if (!['1','2','3','4','5'].includes(operation)) {
+    notice("Sorry, that's not a valid choice.");
     continue;
   }
 
-  console.log("What's the first number?");
+  prompt("What's the first number?");
   number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    notice("Sorry, that's not a valid number.");
+    prompt("What's the first number?");
+    number1 = readline.question();
+  }
   
-  console.log("What's the second number?");
+  prompt("What's the second number?");
   number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    notice("Sorry, that's not a valid number.");
+    prompt("What's the second number?");
+    number2 = readline.question();
+  }
   
+  //change input strings to numbers
   number1 = Number(number1);
   number2 = Number(number2);
   
   output = undefined;
-  
-  if (operation === '1') {
-    output = number1 + number2;
-  } else if (operation === '2') {
-    output = number1 - number2;
-  } else if (operation === '3') {
-    output = number1 * number2;
-  } else if (operation === '4') {
-    output = number1 / number2;
-  } 
+
+  switch (operation) {
+    case '1':
+      output = number1 + number2;
+      break;
+    case '2':
+      output = number1 - number2;
+      break;
+    case '3':
+      output = number1 * number2;
+      break;
+    case '4':
+      output = number1 / number2;
+      break;
+  }
   
   if (output) {
     console.log(`The result is: ${output}`);
   }
+}
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function notice(message) {
+  console.log(`-----${message}-----`)
+}
+
+function invalidNumber(input) {
+  return input.trimStart() === '' || Number.isNaN(Number(input));
 }
 
