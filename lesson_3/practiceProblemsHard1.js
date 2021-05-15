b44141f3ca4280d5d196a5a4acd6213fb7bd3673
Ssnow-dev <ssnow86.dev@gmail.com>
2021-05-15 13:02:09 -0700

    finish practiceProblemsHard1.js

---
+++ lesson_3/practiceProblemsHard1.js
@@ -0,0 +1,42 @@
+//Q1
+//no
+
+//Q2
+//{ first: [1, 2] }
+
+//Q3
+//A) ["one"], ["two"], ["three"]
+//B) ["one"], ["two"], ["three"]
+//C) ["two"], ["three"], ["one"]
+
+//Q4
+console.log(isDotSeparatedIpAddress('10.4.5.11'));
+console.log(isDotSeparatedIpAddress('1.2.3.4.5'));
+console.log(isDotSeparatedIpAddress('4.5.5'));
+console.log(isDotSeparatedIpAddress('4.54.75.345'));
+
+function isDotSeparatedIpAddress(inputString) {
+  let dotSeparatedWords = inputString.split(".");
+  if (dotSeparatedWords.length !== 4) {
+    return false;
+  }
+  while (dotSeparatedWords.length > 0) {
+    let word = dotSeparatedWords.pop();
+    if (!isAnIpNumber(word)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+function isAnIpNumber(str) {
+  if (/^\d+$/.test(str)) {
+    let number = Number(str);
+    return number >= 0 && number <= 255;
+  }
+
+  return false;
+}
+
