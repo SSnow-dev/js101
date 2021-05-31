5b72e2c0327964cf3f46b2b732c8a7b8c0a0eaf3
Ssnow-dev <ssnow86.dev@gmail.com>
2021-05-30 18:19:53 -0700

    finsih practiceProblems for js101 lesson_5

--- lesson_5/practiceProblems.js
+++ lesson_5/practiceProblems.js
@@ -1,8 +1,10 @@
+console.log('=======================[Question 1]=======================');
 //Q1
 
 let arr = ['10', '11', '9', '7', '8'];
 console.log(arr.sort((a, b) => a - b));
 
+console.log('=======================[Question 2]=======================');
 //Q2
 
 let books = [
@@ -16,6 +18,7 @@ books.sort((a, b) => Number(a.published) - Number(b.published));
 
 console.log(books);
 
+console.log('=======================[Question 3]=======================');
 //Q3
 
 let arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]];
@@ -34,6 +37,7 @@ console.log(arr3[2].third[0][0]);
 console.log(obj1.b[1]);
 console.log(Object.keys(obj2.third)[0]);
 
+console.log('=======================[Question 4]=======================');
 //Q4
 
 arr1 = [1, [2, 3], 4];
@@ -53,6 +57,7 @@ console.log(obj1);
 obj2.a.a[2] = 4;
 console.log(obj2);
 
+console.log('=======================[Question 5]=======================');
 //Q5
 
 let munsters = {
@@ -71,6 +76,8 @@ Object.entries(munsters).forEach(munster => {
 });
 console.log('The total age of all the males is: ' + totalMaleAge);
 console.log();
+
+console.log('=======================[Question 6]=======================');
 //Q6
 
 for (let member in munsters) {
@@ -78,11 +85,13 @@ for (let member in munsters) {
   console.log(`${member} is a ${munster.age}-year-old ${munster.gender}.`)
 }
 
+console.log('=======================[Question 7]=======================');
 //Q7
 
 // a === 2;
 // b === [3, 8];
-
+console.log('a === 2, b === [3, 8]');
+console.log('=======================[Question 8]=======================');
 //Q8
 let obj = {
   first: ['the', 'quick'],
@@ -112,6 +121,7 @@ vowels.forEach(string => {
   }
 });
 
+console.log('=======================[Question 9]=======================');
 //Q9
 arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']];
 // let orderedArr = arr.slice();
@@ -133,6 +143,7 @@ let orderedArr = arr.map(array => {
 console.log(arr);
 console.log(orderedArr);
 
+console.log('=======================[Question 10]=======================');
 //Q10
 
 let orderedArrDecending = arr.map(array => {
@@ -145,6 +156,7 @@ let orderedArrDecending = arr.map(array => {
 console.log(orderedArrDecending);
 console.log();
 
+console.log('=======================[Question 11]=======================');
 //Q11
 
 arr = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }];
@@ -163,6 +175,7 @@ console.log();
 console.log(inrementedObj);
 console.log(arr);
 
+console.log('=======================[Question 12]=======================');
 //Q12
 console.log();
 arr = [[2], [3, 5, 7], [9], [11, 15, 18]];
@@ -172,6 +185,7 @@ let multiplesOfThree = arr.map(numArr => {
 });
 console.log(multiplesOfThree);
 
+console.log('=======================[Question 13]=======================');
 //Q13
 console.log();
 arr = [[1, 6, 7], [1, 5, 3], [1, 8, 3]];
@@ -193,6 +207,101 @@ arr.sort((a, b) => {
 });
 
 console.log(arr);
-
+console.log('=======================[Question 14]=======================');
 //Q14
 
+obj = {
+  grape: { type: 'fruit', colors: ['red', 'green'], size: 'small' },
+  carrot: { type: 'vegetable', colors: ['orange'], size: 'medium' },
+  apple: { type: 'fruit', colors: ['red', 'green'], size: 'medium' },
+  apricot: { type: 'fruit', colors: ['orange'], size: 'medium' },
+  marrow: { type: 'vegetable', colors: ['green'], size: 'large' },
+};
+
+let array = [];
+for (let plant in obj) {
+  for (let key in obj[plant]) {
+    if (obj[plant].type === 'fruit' && key === 'colors') {
+      let colors = obj[plant][key];
+      array.push(colors.map(color => {
+        return color[0].toUpperCase() + color.slice(1);
+      }));
+    }
+    if (obj[plant].type === 'vegetable' && key === 'size') {
+      let size = obj[plant][key];
+      array.push(size.toUpperCase());
+    }
+  }
+}
+console.log(array);
+
+console.log('=======================[Question 15]=======================');
+//Q15
+
+arr = [
+  { a: [1, 2, 3] },
+  { b: [2, 4, 6], c: [3, 6], d: [4] },
+  { e: [8], f: [6, 10] },
+];
+
+let evenObjects = arr.filter(obj => {
+  return Object.values(obj).every(subArr => {
+    return subArr.every(number => {
+      return number % 2 === 0;
+    });
+  });
+});
+console.log(evenObjects);
+
+console.log('=======================[Question 16]=======================');
+//Q16
+
+arr = [['a', 1], ['b', 'two'], ['sea', {'c': 3}], ['D', ['a', 'b', 'c']]];
+
+// expected return value of function call
+// { a: 1, b: 'two', sea: { c: 3 }, D: [ 'a', 'b', 'c' ] }
+
+let newObj = {};
+arr.forEach(array => {
+  newObj[array[0]] = array[1];
+});
+
+console.log(newObj);
+
+console.log('=======================[Question 17]=======================');
+//Q17
+
+function generateUUID() {
+  const POSSIBLE_VALUES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  function randomHexNumber() {
+    let index = Math.floor(Math.random() * POSSIBLE_VALUES.length);
+    return POSSIBLE_VALUES[index];
+    }
+    
+    function buildPart(string, length) {
+      for (let i = 0; i < length; i++) {
+        string += randomHexNumber();
+      }
+      return string;
+    }
+
+    let firstPart = '';
+    let secondPart = '';
+    let thirdPart = '';
+    let fourthPart = '';
+    let fifthPart = '';
+
+    firstPart = buildPart(firstPart, 8);
+    secondPart = buildPart(secondPart, 4);
+    thirdPart = buildPart(thirdPart, 4);
+    fourthPart = buildPart(fourthPart, 4);
+    fifthPart = buildPart(fifthPart, 12);
+
+    finalString = `${firstPart}-${secondPart}-${thirdPart}-${fourthPart}-${fifthPart}`;
+    return finalString;
+}
+
+for (let i = 0; i < 10; i++){
+  console.log(generateUUID());
+}
