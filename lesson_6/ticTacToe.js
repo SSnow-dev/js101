abfd275d4e5f266d815e4776ac2b08efc24ac3a3
Ssnow-dev <ssnow86.dev@gmail.com>
2021-06-01 13:27:25 -0700

    add some of the bonus features to ticTacToe, strengthen computer AI

--- lesson_6/ticTacToe.js
+++ lesson_6/ticTacToe.js
@@ -2,34 +2,52 @@ const readline = require('readline-sync');
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
+const NUMBER_OF_MATCHES_TO_WIN = 5;
 
-while (true) {
-  let board = initializeBoard();
+while (true)
+{
+  let score = initializeScore();
 
   while (true) {
+    let board = initializeBoard();
+  
+    while (true) {
+      displayGame(board, score);
+
+      playerChoosesSquare(board);
+      if (someoneWon(board) || boardFull(board)) break;
+    
+      computerChoosesSquare(board);
+      if (someoneWon(board) || boardFull(board)) break;
+    }
+    
+    console.clear();
     displayBoard(board);
-  
-    playerChoosesSquare(board);
-    if (someoneWon(board) || boardFull(board)) break;
-  
-    computerChoosesSquare(board);
-    if (someoneWon(board) || boardFull(board)) break;
-  }
-  
-  displayBoard(board);
-  
-  if (someoneWon(board)) {
-    prompt(`${detectWinner(board)} won!`);
-  } else {
-    prompt("It's a tie!");
+
+    if (someoneWon(board)) {
+      message(`${detectWinner(board)} won!`);
+      handleWin(detectWinner(board), score);
+    } else {
+      prompt("It's a tie!");
+    }
+
+    displayScore(score);
+
+    if (fiveMatchesWon(score)) {
+      message(`${detectFiveMatchWinner(score)} won 5 matches and wins the game!`);
+      break;
+    } else {
+      prompt('Press enter to start next match.');
+      readline.prompt('');
+    }
   }
 
-  prompt('Play again? (y or n)');
-  let answer = readline.question().toLowerCase()[0];
-  if (answer !== 'y') break;
+  
+  if (!playAgain()) break;
 }
 
-prompt('Thanks for playing Tic Tac Toe!');
+
+message('Thanks for playing Tic Tac Toe!');
 
 
 function initializeBoard() {
@@ -43,8 +61,6 @@ function initializeBoard() {
 }
 
 function displayBoard(board) {
-  console.clear();
-
   console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
 
   console.log('');
@@ -66,7 +82,7 @@ function playerChoosesSquare(board) {
   let square;
 
   while (true) {
-    prompt(`Choose a square (${emptySquares(board).join(', ')}):`);
+    prompt(`Choose a square (${joinOr(emptySquares(board))}):`);
     square = readline.question().trim();
 
     if (emptySquares(board).includes(square)) break;
@@ -79,13 +95,29 @@ function playerChoosesSquare(board) {
 
 function computerChoosesSquare(board) {
   let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+  let threat = detectImmediateThreat(board);
+  let winningSqaure = detectImmediateWin(board);
+  let square;
 
-  let square = emptySquares(board)[randomIndex];
+  if (winningSqaure !== -1) {
+    square = winningSqaure;
+  } else if (threat !== -1) {
+    square = threat;
+  } else if (board['5'] === ' ') {
+    square = 5;
+  } else {
+    square = emptySquares(board)[randomIndex];
+  }
+
   board[square] = COMPUTER_MARKER;
 }
 
 function prompt(string) {
   console.log(`==> ${string}`);
+}
+
+function message(string) {
+  console.log(`***** ${string} *****`);
 }
 
 function emptySquares(board) {
@@ -128,4 +160,138 @@ function detectWinner(board) {
   return null;
 }
 
+function joinOr(array, seperator = ', ', string = 'or') {
+  if (array.length <= 1) {
+    return array.join('');
+  }
+  let finalString = '';
+  let lastElement = array.length - 1;
 
+  finalString = array.slice(0, lastElement).join(seperator);
+  finalString = finalString.concat('', `${seperator}${string} ${array[lastElement]}`)
+  return finalString;
+}
+
+function handleWin(winner, score) {
+  if (winner === 'Player') {
+    score['playerWins'] += 1;
+  } else if (winner === 'Computer') {
+    score['computerWins'] += 1;
+  }
+}
+
+function displayScore(score) {
+  prompt(`The score is: Player: ${score.playerWins} Computer: ${score.computerWins}`);
+}
+
+function initializeScore() {
+  return {
+    playerWins: 0,
+    computerWins: 0
+  };
+}
+
+function detectFiveMatchWinner(score) {
+  if (score.playerWins >= 5) {
+    return 'Player';
+  } else if (score.computerWins >= 5) {
+    return 'Computer';
+  }
+}
+
+function fiveMatchesWon(score) {
+  return !!detectFiveMatchWinner(score)
+}
+
+function displayGame(board, score) {
+  console.clear();
+  displayBoard(board);
+  displayScore(score);
+}
+
+function detectImmediateThreat(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], 
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], 
+    [1, 5, 9], [3, 5, 7]  
+  ];
+  let immediateThreat = -1;
+
+  for (let line = 0; line < winningLines.length; line++) {
+    let [ sq1, sq2, sq3 ] = winningLines[line];
+
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === ' '
+    ) {
+      immediateThreat = sq3;
+    }
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === ' ' &&
+      board[sq3] === HUMAN_MARKER
+    ) {
+      immediateThreat = sq2;
+    }
+    if (
+      board[sq1] === ' ' &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === HUMAN_MARKER
+    ) {
+      immediateThreat = sq1;
+    }
+  }
+  return immediateThreat;
+}
+
+function detectImmediateWin(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], 
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], 
+    [1, 5, 9], [3, 5, 7]  
+  ];
+  let immediateWin = -1;
+
+  for (let line = 0; line < winningLines.length; line++) {
+    let [ sq1, sq2, sq3 ] = winningLines[line];
+
+    if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === ' '
+    ) {
+      immediateWin = sq3;
+    }
+    if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === ' ' &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      immediateWin = sq2;
+    }
+    if (
+      board[sq1] === ' ' &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      immediateWin = sq1;
+    }
+  }
+  return immediateWin;
+}
+
+function playAgain() {
+  let answer;
+  while (true) {
+    prompt('Play again? (y or n)');
+    answer = readline.question().toLowerCase();
+    if (answer !== 'y' && answer !== 'n') {
+      prompt("Sorry thats not a valid choice.")
+    } else {
+      break;
+    }
+  }
+  if (answer === 'y') return true;
+  if (answer === 'n') return false;
+}
