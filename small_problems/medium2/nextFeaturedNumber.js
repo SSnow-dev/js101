cd94f31a60a044cf88c0b8e1d9c16f3eb31d0a8d
Ssnow-dev <ssnow86.dev@gmail.com>
2021-06-04 13:29:06 -0700

    continue work on js101 small problems medium2

--- small_problems/medium2/nextFeaturedNumber.js
+++ small_problems/medium2/nextFeaturedNumber.js
@@ -1,59 +1,31 @@
-const fs = require('fs');
-const featuredNumbers = require('./featuredNumbers.json');
+const LARGEST_FEATURED_NUMBER = 9876543201;
 
-featured(12);           // 21
-featured(20);           // 21
-featured(21);           // 35
-featured(997);          // 1029
-featured(1029);         // 1043
-featured(999999);       // 1023547
-featured(999999987);    // 1023456987
-featured(9876543186);   // 9876543201
-featured(9876543200);   // 9876543201
-featured(9876543201);   // "There is no possible number that fulfills those requirements."
+console.log(featured(12));           // 21
+console.log(featured(20));           // 21
+console.log(featured(21));           // 35
+console.log(featured(997));          // 1029
+console.log(featured(1029));         // 1043
+console.log(featured(999999));       // 1023547
+console.log(featured(999999987));    // 1023456987
+console.log(featured(9876543186));   // 9876543201
+console.log(featured(9876543200));   // 9876543201
+console.log(featured(9876543201));   // "There is no possible number that fulfills those requirements."
 
 function featured(num) {
-
-}
-// console.log(featuredNumbers[3]);
-// console.log(Number(featuredNumbers[3]));
-
-let answer;
-let test = 99999;
-for (let i = 0; i <= 9876543201; i++) {
-  if (featuredNumbers[i] > test) {
-    answer = featuredNumbers[i];
-    break;
+  let answer = 0;
+  let incrementer = 1;
+  for (let startNum = num; startNum <= LARGEST_FEATURED_NUMBER; startNum += incrementer) {
+    //short circit if we already changed incrementer to avoid extra function calls.
+    if (incrementer != 14 && isOdd(startNum) && isMultipleOfSeven(startNum)) {
+      incrementer = 14;
+    }
+    if (startNum > num && isFeaturedNumber(startNum)) {
+      answer = startNum;
+      return answer;
+    }
   }
+  return "There is no possible number that fulfills those requirements.";
 }
-console.log(answer);
-
-
-// let testNum = 49;
-// console.log(featuredNumbers.includes(testNum));
-// testNum = 21;
-// console.log(uniqueDigits(testNum) && isOdd(testNum) && isMultipleOfSeven(testNum));
-// testNum = 35;
-// console.log(uniqueDigits(testNum) && isOdd(testNum) && isMultipleOfSeven(testNum));
-// testNum = 9876543201;
-// console.log(uniqueDigits(testNum) && isOdd(testNum) && isMultipleOfSeven(testNum));
-
-
-// let allFeaturedNums = [];
-// for (let i = 0; i <= 987654; i++) {
-//   if (isFeaturedNumber(i)) {
-//     allFeaturedNums.push(i);
-//   }
-// }
-
-// fs.writeFile('./featuredNumbers.json', JSON.stringify(allFeaturedNums), err => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log('File successfully written!');
-//   }
-// });
-
 
 function uniqueDigits(num) {
   if (num < 10 && num > -10) {
---
+++ small_problems/medium2/sumSquareMinusSquareSum.js
@@ -0,0 +1,16 @@
+console.log(sumSquareDifference(3));      // 22 --> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+console.log(sumSquareDifference(10));     // 2640
+console.log(sumSquareDifference(1));      // 0
+console.log(sumSquareDifference(100));    // 25164150
+
+function sumSquareDifference(num) {
+
+}
+
+function squareOfSum(num) {
+
+}
+
+function sumOfSquare(num) {
+  
+}
