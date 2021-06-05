54bd3a439be22dfc2c8d90cd965024bb2b0644db
Ssnow-dev <ssnow86.dev@gmail.com>
2021-06-04 22:08:56 -0700

    work on js101 small problems medium2

---
+++ small_problems/medium2/bubbleSort.js
@@ -0,0 +1,26 @@
+let array1 = [5, 3];
+bubbleSort(array1);
+console.log(array1);    // [3, 5]
+
+let array2 = [6, 2, 7, 1, 4];
+bubbleSort(array2);
+console.log(array2);    // [1, 2, 4, 6, 7]
+
+let array3 = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie'];
+bubbleSort(array3);
+console.log(array3);    // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
+
+function bubbleSort(array) {
+  while (true) {
+    let didSwap = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        didSwap = true;
+      }
+    }
+    if (didSwap === false) break;
+  }
+}
--- small_problems/medium2/sumSquareMinusSquareSum.js
+++ small_problems/medium2/sumSquareMinusSquareSum.js
@@ -3,14 +3,23 @@ console.log(sumSquareDifference(10));     // 2640
 console.log(sumSquareDifference(1));      // 0
 console.log(sumSquareDifference(100));    // 25164150
 
-function sumSquareDifference(num) {
 
+function sumSquareDifference(num) {
+  return squareOfSum(num) - sumOfSquare(num);
 }
 
 function squareOfSum(num) {
-
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum**2;
 }
 
 function sumOfSquare(num) {
-  
+  let total = 0;
+  for (let i = 0; i <= num; i++) {
+    total += i**2;
+  }
+  return total;
 }
